6039ec31e0cacd2b093678cfc6585375a4402ca9
XdRonaldoxD <37567931+XdRonaldoxD@users.noreply.github.com>
2024-07-21 18:57:14 -0500

    token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  key_token:'',
+  key_token:'@TEXCOTTOMDESING2021LS~$',
   apiredParts: 'https://api.sistemaboticarosa.com/?dominio=sistemaboticarosa',
   apiGPT:''
 };
