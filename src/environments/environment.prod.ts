1916be3f2e7117689fc940bfcddc4143993604e6
XdRonaldoxD <37567931+XdRonaldoxD@users.noreply.github.com>
2024-07-21 23:16:39 -0500

    ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   key_token:'@TEXCOTTOMDESING2021LS~$',
   apiredParts: 'https://api.sistemaboticarosa.com/?dominio=sistemaboticarosa',
-  apiGPT:''
+  apiGPT:""
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   key_token:'@TEXCOTTOMDESING2021LS~$',
   apiredParts: 'https://api.sistemaboticarosa.com/?dominio=sistemaboticarosa',
-  apiGPT:''
+  apiGPT:""
 };
 
 
