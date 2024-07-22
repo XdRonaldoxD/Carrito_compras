c805cf6bec9a699be3aa0708458c4797d22c064e
XdRonaldoxD <37567931+XdRonaldoxD@users.noreply.github.com>
2024-07-21 19:05:55 -0500

    creacion archivo redirect para netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   key_token:'@TEXCOTTOMDESING2021LS~$',
   apiredParts: 'https://api.sistemaboticarosa.com/?dominio=sistemaboticarosa',
   apiGPT:''
